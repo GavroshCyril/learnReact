a2cf3ac3f39e4eff250e9280601d81bd55e62c0a
Gavrosh Cyril <gavrosh.cyril@gmail.com>
2024-03-17 21:41:01 +0300

    feat: create card feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import "./App.css";
+import "./App.scss";
 import { LargeCard } from "./components";
+import astronoutImage from "./assets/astronout.jpg";
 
 function App() {
+  const cardInformation = {
+    date: "January 1, 2021",
+    title: "Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
+    description:
+      "Astronauts Kayta Barron and Rale Char fioated out of the international Space Station airlock for a spacewalk Tuesday, Instaling brackets and struts to support new solar arrays to upgrade the research tabs power system ci the same day that cremate Mark Vande He marked his 341st day in orbit, a UIS, record for a singie spacelight.",
+    imageUrl: astronoutImage,
+  };
+
   return (
-    <>
+    <div className="" style={{ margin: "16px" }}>
       <LargeCard
-        date="January 1, 2021"
-        title="Hello"
-        description="World"
-        image="https://picsum.photos/200/300"
+        date={cardInformation.date}
+        title={cardInformation.title}
+        description={cardInformation.description}
+        image={cardInformation.imageUrl}
       />
-    </>
+    </div>
   );
 }
 
--- src/components/largeCard/largeCard.tsx
+++ src/components/largeCard/largeCard.tsx
@@ -1,4 +1,5 @@
 import "./largeCard.scss";
+import { FeedbackSection } from "../feedbackSection/feedbackSection";
 
 interface LargeCardProps {
   date: string;
@@ -7,12 +8,21 @@ interface LargeCardProps {
   image: string;
 }
 
-export const LargeCard = ({ date, title, description }: LargeCardProps) => {
+export const LargeCard = ({ date, title, description, image }: LargeCardProps) => {
   return (
-    <div className="large-card large-card__container">
-      <span className="large-card__date">{date}</span>
-      <span className="large-card__title">{title}</span>
-      <div className="large-card__description">{description}</div>
+    <div className="large-card">
+      <div className="large-card__container">
+        <div className="large-card__info">
+          <span className="large-card__date">{date}</span>
+          <span className="large-card__title">{title}</span>
+          <p className="large-card__description">{description}</p>
+        </div>
+        <div className="large-card__image">
+          <img src={image} alt={title} />
+        </div>
+      </div>
+
+      <FeedbackSection />
     </div>
   );
 };
